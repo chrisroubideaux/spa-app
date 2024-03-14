4d3f433f5d17e6812d641d31944cf84de78007b1
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-03-14 08:51:41 -0500

    code update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const authRoutes = express.Router();
 const passport = require('passport');
 const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const { Strategy: FacebookStrategy } = require('passport-facebook');
 
 require('dotenv').config();
 
@@ -52,6 +53,76 @@ authRoutes.get(
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
     // Redirect to profile page after successful authentication
+    res.redirect('/profile');
+  }
+);
+
+// facebook passport oAuth
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL:
+        process.env.FACEBOOK_CALLBACK_URL ||
+        'https://ivy-database-87df4cfe65bb.herokuapp.com/auth/google/callback',
+      profileFields: ['id', 'displayName', 'photos', 'emails'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log('Facebook Profile Data:', profile);
+
+      try {
+        // Check if the Facebook user is already registered in your database
+        const existingUser = await User.findOne({ 'facebook.id': profile.id });
+
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        // Create a new user with Facebook account details
+        const newUser = new User({
+          facebook: {
+            id: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value,
+          },
+        });
+
+        await newUser.save();
+
+        return done(null, newUser);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// Facebook OAuth login route
+authRoutes.get(
+  '/auth/facebook',
+  passport.authenticate('facebook', { scope: ['email'] })
+);
+
+// Facebook OAuth callback route
+authRoutes.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
     res.redirect('/profile');
   }
 );
