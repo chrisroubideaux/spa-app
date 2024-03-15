b85ae84ab4a997b45f20616aee76b3b6aee1f60f
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-03-15 13:53:17 -0500

    code update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,35 +1,20 @@
 // user route crud operations
 const express = require('express');
-const authRoutes = express.Router();
-
-const User = require('../models/user'); // Import User model
-
+const userRoutes = express.Router();
 const {
   getUserById,
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
-
-// Google OAuth callback route
-
-// POST (create) new user profile route
-authRoutes.post('/', async (req, res) => {
-  try {
-    const { email, fullName } = req.body;
-    const newUser = await User.create({ email, fullName }); // Create new user using User model
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error('Error creating new user profile:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+const Users = require('../models/users');
 
 // GET user profile page (protected route)
-authRoutes.get('/:id', async (req, res) => {
+userRoutes.get('/:id', async (req, res) => {
   try {
     const userId = req.params.id;
-    const userData = await getUserById(userId); // Use getUserById function to fetch user data
-    res.status(200).json(userData);
+    console.log('User ID:', userId);
+    const userData = await Users.findById(userId);
+    res.status(200).json(userData); // Send user data back to the client
   } catch (error) {
     console.error('Error fetching user profile by ID:', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -37,12 +22,12 @@ authRoutes.get('/:id', async (req, res) => {
 });
 
 // PUT (update) user profile (protected route)
-authRoutes.put('/:id', async (req, res) => {
+userRoutes.put('/:id', async (req, res) => {
   try {
     const userId = req.params.id;
     const { fullName, email, newPassword } = req.body;
 
-    await updateUser(userId, fullName, email, newPassword); // Use updateUser function to update user data
+    await updateUser(userId, fullName, email, newPassword);
 
     res.status(200).json({ message: 'User profile updated successfully' });
   } catch (error) {
@@ -52,10 +37,12 @@ authRoutes.put('/:id', async (req, res) => {
 });
 
 // DELETE user profile (protected route)
-authRoutes.delete('/:id', async (req, res) => {
+userRoutes.delete('/:id', async (req, res) => {
   try {
     const userId = req.params.id;
-    await deleteUser(userId); // Use deleteUser function to delete user data
+
+    await deleteUser(userId);
+
     res.status(200).json({ message: 'User profile deleted successfully' });
   } catch (error) {
     console.error('Error deleting user profile:', error);
@@ -63,4 +50,4 @@ authRoutes.delete('/:id', async (req, res) => {
   }
 });
 
-module.exports = authRoutes;
+module.exports = userRoutes;
